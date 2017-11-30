b8c797bc7610df6c707823bd45169705088245ab
erinknight <erin.knight@headspring.com>
2017-11-29 23:29:32 -0600

    add CORS handling

--- server.babel.js
+++ server.babel.js
@@ -1,8 +1,21 @@
 import express from 'express';
 import getData from './getData';
 
+const allowCrossDomain = (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'hsbot-monopoly-board.azurewebsites.net');
+  res.header('Access-Control-Allow-Methods', 'GET,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+};
+
 const app = express();
 
+app.use(allowCrossDomain);
 app.use('/', express.static('public'));
 app.use('/get', getData());
 
