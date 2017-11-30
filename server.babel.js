3f66bccb6fd78b2a68a3984be77673429ddd21e6
erinknight <erin.knight@headspring.com>
2017-11-30 13:07:29 -0600

    Adds npm build command, and another attempted CORS update

--- server.babel.js
+++ server.babel.js
@@ -5,12 +5,7 @@ const allowCrossDomain = (req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-
-  if (req.method === 'OPTIONS') {
-    res.send(200);
-  } else {
-    next();
-  }
+  next();
 };
 
 const app = express();
@@ -20,3 +15,4 @@ app.use('/', express.static('public'));
 app.use('/get', getData());
 
 app.listen(process.env.PORT || 3000);
+console.log(`Listening on port ${process.env.PORT || 3000}...`);
